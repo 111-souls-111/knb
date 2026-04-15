7a7112ac9684cf0aeeb7ed2bd3e835b39e72ccdb
maxim melnikov <mmelnikov750@gmail.com>
2026-04-15 03:02:33 +0300

    comments useCamera

--- src/Hooks/useCamera.js
+++ src/Hooks/useCamera.js
@@ -2,21 +2,24 @@ import { useState, useRef, useCallBack, useEffect } from "react";
 
 
 export const useCamera = () => {
+    //позже положу сюда камеру UseRef чтобы каждый раз заново не отрисовывать
     const webcamRef = useRef(null)
-    const [isCameraReady, seyIsCameraReady] = useState(false);
-    const [cameraError, setCameraError] = useState(null);
+    const [isCameraReady, seyIsCameraReady] = useState(false); //готовность камеры
+    const [cameraError, setCameraError] = useState(null); //ошибка
 
+
+    //параметры для камеры
     const videoConstraints = {
         width: 640,
         height: 480,
         facingmode: 'User'
     };
-
+    //когда ошибка при подготовке камеры
     const handleError = useCallBack((error) => {
         setCameraError(error.message)
         setCameraReady(false)
     }, []);
-
+    //когда камера готова
     const handleUserMedia = useCallBack(() => {
         setCameraError(null)
         setCameraReady(true)
