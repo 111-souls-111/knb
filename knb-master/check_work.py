98e73b134dbce52c3f216a73c1a21224c55aaa09
maxim melnikov <mmelnikov750@gmail.com>
2026-04-26 20:35:35 +0300

    server

--- knb-master/check_work.py
+++ knb-master/check_work.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS 
 from database.auth import init_db, add_user, get_user
 from pasword.secur import hash_password, verify_password, validate_credentials
 
 app = Flask(__name__)
+CORS(app) 
 
 
 @app.route("/")
